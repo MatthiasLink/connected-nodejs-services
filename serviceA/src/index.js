187a9ff274bf3addfc3dd748539f4d7acba4e668
m-link-arctur <matthias.link@arctur.de>
2021-12-29 19:57:12 +0100

    reuse serviceB in serviceA

--- serviceA/src/index.js
+++ serviceA/src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const axios = require('axios');
 
 const PORT = 8080;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("serviceA");
+app.get("/", async (req, res) => {
+    const { data: serviceB } = await axios.get('http://service-b:8080/', { responseType: "text" });
+    res.send("serviceA + " + serviceB);
 });
 
 app.listen(PORT, () => {
