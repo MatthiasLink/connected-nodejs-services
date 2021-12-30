fa3925c6f368c08ecf7b26900bb9cadceaf33d2e
m-link-arctur <matthias.link@arctur.de>
2021-12-30 10:27:37 +0100

    connect services within kubernetes with env

--- serviceA/src/index.js
+++ serviceA/src/index.js
@@ -4,9 +4,18 @@ const axios = require('axios');
 const PORT = 8080;
 const app = express();
 
+const serviceBHost = process.env.SERVICE_B_SERVICE_SERVICE_HOST || "service-b";
+const serviceBPort = process.env.SERVICE_B_SERVICE_SERVICE_PORT || "8080";
+
 app.get("/", async (req, res) => {
-    const { data: serviceB } = await axios.get('http://service-b:8080/', { responseType: "text" });
-    res.send("serviceA + " + serviceB);
+    console.log(JSON.stringify(process.env));
+    try {
+        const { data: serviceB } = await axios.get(`http://${serviceBHost}:${serviceBPort}/`, { responseType: "text" });
+        res.send("serviceA + " + serviceB);
+    } catch (e) {
+        console.log(e);
+        res.send("serviceB not reached");
+    }
 });
 
 app.listen(PORT, () => {
