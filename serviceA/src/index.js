5b6ddda2f35bb4b8879a42c75e2cdb9dfe7b0032
m-link-arctur <matthias.link@arctur.de>
2021-12-30 12:38:33 +0100

    use environment variables in docker case

--- serviceA/src/index.js
+++ serviceA/src/index.js
@@ -4,11 +4,15 @@ const axios = require('axios');
 const PORT = 8080;
 const app = express();
 
-const serviceBHost = process.env.SERVICE_B_SERVICE_SERVICE_HOST || "service-b";
-const serviceBPort = process.env.SERVICE_B_SERVICE_SERVICE_PORT || "8080";
+const serviceBHost = process.env.SERVICE_B_SERVICE_SERVICE_HOST;
+const serviceBPort = process.env.SERVICE_B_SERVICE_SERVICE_PORT;
+
+if (!serviceBPort || !serviceBPort) {
+    res.send("env variables have to be specified");
+    return process.exit(22);
+}
 
 app.get("/", async (req, res) => {
-    console.log(JSON.stringify(process.env));
     try {
         const { data: serviceB } = await axios.get(`http://${serviceBHost}:${serviceBPort}/`, { responseType: "text" });
         res.send("serviceA + " + serviceB);
