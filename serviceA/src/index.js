b08ea360e7d307c5d57a64d2588cac2d3eec0bbb
m-link-arctur <matthias.link@arctur.de>
2021-12-30 13:32:32 +0100

    allow all modes with env variables and default

--- serviceA/src/index.js
+++ serviceA/src/index.js
@@ -4,13 +4,8 @@ const axios = require('axios');
 const PORT = 8080;
 const app = express();
 
-const serviceBHost = process.env.SERVICE_B_SERVICE_SERVICE_HOST;
-const serviceBPort = process.env.SERVICE_B_SERVICE_SERVICE_PORT;
-
-if (!serviceBPort || !serviceBPort) {
-    res.send("env variables have to be specified");
-    return process.exit(22);
-}
+const serviceBHost = process.env.SERVICE_B_SERVICE_SERVICE_HOST || "localhost";
+const serviceBPort = process.env.SERVICE_B_SERVICE_SERVICE_PORT || "8081";
 
 app.get("/", async (req, res) => {
     try {
